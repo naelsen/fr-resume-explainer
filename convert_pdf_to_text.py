430f6614c698c6fa7be166a4549a96bf2e6cc1a4
naelsen <nael.sennoun@gmail.com>
2023-03-08 12:31:35 +0100

    Ajout de la lib permettant de convertir les pdf en texte

---
+++ convert_pdf_to_text.py
@@ -0,0 +1,35 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+import re
+
+def convert_pdf_to_text(file_path):
+    
+    # On crée un gestionnaire de ressources PDF
+    resource_manager = PDFResourceManager()
+    
+    # On crée un tampon de texte en sortie
+    output_string = StringIO()
+    
+    # On crée un convertisseur de PDF en texte
+    converter = TextConverter(resource_manager, output_string, laparams=LAParams())
+
+    # On crée un interpréteur de pages PDF
+    interpreter = PDFPageInterpreter(resource_manager, converter)
+    
+    # On ouvre le fichier PDF en mode binaire
+    with open(file_path, 'rb') as file:
+        # On traite chaque page du PDF
+        for page in PDFPage.get_pages(file):
+            interpreter.process_page(page)
+            
+    # On récupère le texte converti
+    text = output_string.getvalue()
+    text = ' '.join(re.findall("(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+|[\w']+", text))
+
+    # On ferme le tampon de sortie
+    output_string.close()
+    
+    return text
